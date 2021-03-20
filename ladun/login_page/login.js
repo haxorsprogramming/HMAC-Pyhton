a85155c24103d36e9b22c06007666082c59e44a5
haxorsprogramming <haxorsuinsu@gmail.com>
2021-03-20 14:38:11 +0700

    update

--- ladun/login_page/login.js
+++ ladun/login_page/login.js
@@ -15,5 +15,22 @@ var divLogin = new Vue({
         }
     }
 });
-
+$('#divImgAwal').hide();
 document.querySelector('#txtUsername').focus();
+// Webcam.set({
+//     width: 320,
+//     height: 240,
+//     image_format:'jpeg',
+//     jpeg_quality: 90
+// });
+// Webcam.attach('#my_camera');
+
+// function take_snapshot() {
+//     // take snapshot and get image data
+//     Webcam.snap( function(data_uri) {
+//         // display results in page
+//         document.getElementById('results').innerHTML = 
+//             '<h2>Here is your image:</h2>' + 
+//             '<img src="'+data_uri+'"/>';
+//     } );
+// }
